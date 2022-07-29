095588fbec5f04edbe866a946fd4df694412ef4c
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2022-07-29 09:57:01 +0430

    test

--- Part10EfDemo/Program.cs
+++ Part10EfDemo/Program.cs
@@ -11,6 +11,7 @@ namespace Part10EfDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
